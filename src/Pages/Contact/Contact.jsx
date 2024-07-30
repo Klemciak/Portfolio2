a42c548b6fa9b2e3ed5339b1d0714ad4d72d518d
Klemciak <klemcio@interia.pl>
2024-07-30 21:57:16 +0200

    finish contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Contact.scss";
+import postbox from "../../Images/postbox.png";
+import facebook from "../../Images/facebook.png";
+import Navbar from "../../Components/Navbar/Navbar";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact-wrap">
+      <Navbar />
+      <h2 className="contact-title">kontakt</h2>
+      <div className="contacts">
+        <div className="facebook">
+          <a href="https://www.facebook.com/K24C01">facebook.com/K24C01</a>
+          <img src={facebook} alt="neon facebook F" />
+        </div>
+        <div className="email">
+          <a href="mailto:klemcio@interia.pl">klemcio@interia.pl</a>
+          <img src={postbox} alt="neon postbox" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
